f380be4017e7a50791433f98269f0b7aba0b7d12
dgikonyo <denniskimani05@gmail.com>
2020-06-27 01:40:51 +0300

    additions

---
+++ models/Disaster.js
@@ -0,0 +1,27 @@
+const mongoose=require('mongoose');
+
+const DisasterSchema=mongoose.Schema({//will describe how our data looks like
+    disasterName:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    dateDiscovered:{
+        type:Date,
+        default:Date.now
+    },
+    responseStatus:{
+        type:String,
+        default: "Not addressed"
+    },
+
+});
+
+module.exports=mongoose.model('Disaster',DisasterSchema);
